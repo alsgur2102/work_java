006be5096c980217de64f0b98abdb34455250a1e
KOITT <KOITT@KOITT-PC>
2018-05-26 14:21:19 +0900

    2장 진단문제 끝까지 완료

--- ch02/src/ch02/Ch02Ex02_04.java
+++ ch02/src/ch02/Ch02Ex02_04.java
@@ -5,19 +5,23 @@ import java.util.Scanner;
 public class Ch02Ex02_04 {
 
 	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-		// 1. 숫자 2개 입력받기
-		String input = scanner.nextLine();
-		String input1 = scanner.nextLine();
-		String input2 = scanner.nextLine();
-		String input3 = scanner.nextLine();
-		// 2. 입력받은 숫자 2개 변수에 저장
-		int num = Integer.parseInt(input);
-		int num1 = Integer.parseInt(input1);
-		int num2 = Integer.parseInt(input2);
-		int num3 = Integer.parseInt(input3);
-		// 3. num - num1 = 계산한 값이 출력되도록 작성
-		System.out.printf("%d - %d = %d%n", num, num1, num - num1);
-		System.out.printf("%d - %d = %d", num2, num3, num2 - num3);
+		// 2.3번 응용
+		
+		// 1. Scanner 객체 생성(System.in은 키보드)
+		Scanner scanner = new Scanner(System.in);
+		
+        // 2. 정수 2개를 문자열로 입력받기
+		String strNum1 = scanner.nextLine();
+		String strNum2 = scanner.nextLine();
+		
+		// 3. 입력받은 정수 2개를 정수(int)형으로 변경하기
+		int num1= Integer.parseInt(strNum1);
+		int num2 = Integer.parseInt(strNum2);
+	
+		// 4. 정수형으로 변경한 후 두 변수 num1과 num2를 이용하여 계산
+		int result = num1 - num2;
+		
+		// 5. num1, num2, result를 출력
+		System.out.printf("%d - %d = %d%n", num1, num2, result); 
 	}
 }
--- ch02/src/ch02/Ch02Ex02_05.java
+++ ch02/src/ch02/Ch02Ex02_05.java
@@ -5,14 +5,21 @@ import java.util.Scanner;
 public class Ch02Ex02_05 {
 
 	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        // 1. Scanner 객체 생성(System.in은 키보드)
+		Scanner scanner = new Scanner(System.in);
         
-        String input = scanner.nextLine();
-		String input1 = scanner.nextLine();
+		// 2. 하나의 정수와 하나의 실수를 문자열로 입력받기
+        String strnum1 = scanner.nextLine();
+		String strnum2 = scanner.nextLine();
 		
-		int num = Integer.parseInt(input);
-		float num1 = Float.parseFloat(input1);
+		// 3. 입력받은 두 문자열을 각각 정수(int)와 실수(float)로 변경하기
+		int num1 = Integer.parseInt(strnum1);
+		float num2 = Float.parseFloat(strnum2);
 		
-		System.out.printf("%d * %f = %f", num, num1, num - num1);
+		// 4. num1, num2 변수를 이용하여 계산하기
+		float result = num1 * num2;
+		
+		// 5. num1, num2, result 변수를 이용하여 출력하기
+		System.out.printf("%d * %f = %f", num1, num2, result);
 	}	
 }
