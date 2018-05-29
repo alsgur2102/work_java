1038984f39202e2bffe08a2e5f8e6d512cc5e12c
KOITT <KOITT@KOITT-PC>
2018-05-29 20:22:10 +0900

    3장 예제 20번까지 완료

--- ch03/src/ch03/OperatorEx11.java
+++ ch03/src/ch03/OperatorEx11.java
@@ -2,11 +2,15 @@ package ch03;
 
 public class OperatorEx11 {
     public static void main(String[] args) {
-		char a = 'a';
-		char d = 'd';
-		char zero = '0';
-		char two = '2';
+    	// 책 102쪽 표 3-8 참고
+    	
+		char a = 'a';    // 아스키 코드 값 97
+		char d = 'd';    // 아스키 코드 값 100
 		
+		char zero = '0';    // 아스키 코드 값 48
+		char two = '2';     // 아스키 코드 값 50
+		
+		// char형은 사칙연산 시, 아스키 코드 값으로 연산한다.
 		System.out.printf("'%c' - '%c' = %d%n", d, a, d - a);  // 'd'-'a'=3
 		System.out.printf("'%c' - '%c' = %d%n", two, zero, two - zero);
 		System.out.printf("'%c'= %d%n", a, (int)a);
