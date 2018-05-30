b8d5dfe131423584ab970b1801a9307b5ce1cb29
KOITT <KOITT@KOITT-PC>
2018-05-30 21:30:07 +0900

    3장 예제 22 수정

--- ch03/src/ch03/OperatorEx22.java
+++ ch03/src/ch03/OperatorEx22.java
@@ -6,11 +6,11 @@ public class OperatorEx22 {
 		double d = 0.1;
 		double d2 = (double)f;
 		
-				// double형과 float형의 비교. 10.0d == 10.0d. 오파발생 X. 결과 값 true
+				// double형과 float형의 비교. 10.0d == 10.0d. 오차발생 X. 결과 값 true
 				System.out.printf("10.0 == 10.0f    %b%n", 10.0 == 10.0f);
 				
 				// double형과 float형의 비교. 0.1d == 0.1d.
-				// float을double로 형변환하면서 오차발생. 결과 값 fales
+				// float을double로 형변환하면서 오차발생. 결과 값 false
 				System.out.printf("0.1 == 0.1f      %b%n", 0.1 == 0.1f);
 				
 				// 총 19자리. 소수점 17자리 표시. 변수 f, d, d2 출력
