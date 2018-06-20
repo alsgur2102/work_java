76d6a6655cb3e55136af9dd856896b3977249a97
KOITT <KOITT@KOITT-PC>
2018-06-20 21:35:57 +0900

    5장 진단문제 1-15까지 완료

--- ch05/src/ch05/Ch05Ex01_02.java
+++ ch05/src/ch05/Ch05Ex01_02.java
@@ -2,6 +2,21 @@ package ch05;
 
 public class Ch05Ex01_02 {
 	public static void main(String[] args) {
-		
+		// [规过 1]
+		int[] numbers01 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for (int i = 0; i < numbers01.length; i++) {
+			System.out.print(numbers01[i] + " ");
+		}
+		System.out.println();
+
+		// [规过 2]
+		int[] numbers02 = new int[10];
+		for (int i = 0; i < numbers02.length; i++) {
+			numbers02[i] = i + 1;
+		}
+
+		for (int i = 0; i < numbers02.length; i++) {
+			System.out.print(numbers02[i] + " ");
+		}
 	}
 }
